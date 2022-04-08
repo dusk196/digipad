279029a7f9058fe52927983c1647eb60ddd21f15
Sayantan Roy <sayantan.roy94@gmail.com>
2022-04-09 00:48:04 +0530

    Let's begin fresh & keep it simple!

--- projects/digipad/src/app/editor/editor.component.ts
+++ projects/digipad/src/app/editor/editor.component.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
 import { faBold, faItalic, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { EditorService } from './editor.service';
-import { EditorUtilsService } from './common/editor-utils.service';
-import { EditorActionsService } from './common/editor-actions.service';
-
-export interface ExecCommandStyle {
-  style: 'color' | 'background-color' | 'font-size' | 'font-weight' | 'font-style' | 'text-decoration';
-  value: string;
-  initial: (element: HTMLElement | null) => Promise<boolean>;
-}
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -19,91 +11,24 @@ export interface ExecCommandStyle {
 
 export class EditorComponent {
 
-  constructor(
-    private _editorService: EditorService,
-    private _editorUtils: EditorUtilsService,
-    private _editorActions: EditorActionsService
-  ) { }
+  constructor(private _editorService: EditorService) { }
 
   faBold: IconDefinition = faBold;
   faItalic: IconDefinition = faItalic
 
   /**
-   * 
-   * @param action Editor actions that to be executed. E.g: 'bold', 'italic', 'underline', 'strikeThrough'
+   * Editor actions that to be executed. E.g: 'bold', 'italic', 'underline' etc.
+   * @param action string
+   * @returns void
    */
-  onEditorAction(action: string) {
+  onEditorAction(action: string): void {
     console.log(action);
-    const actionExists = this._editorActions.checkIfActionExists(action);
-    if (!actionExists) {
-      switch (action) {
-        case 'bold':
-          this._editorActions.executeAction('strong');
-          break;
-        case 'highlight':
-          if (this._editorUtils.isSameElement())
-            this._editorService.getSelection()?.getRangeAt(0).commonAncestorContainer.childNodes.forEach(element => {
-              console.log(element);
-            });
-          break;
-        default:
-          break;
-      }
-    }
-  }
-
-  onBold(): void {
-    const selection = this._editorService.getSelection()?.getRangeAt(0);
-    console.log(selection);
-    const selectedContent = selection?.extractContents();
-    const span = document.createElement('span');
-    span.style.backgroundColor = 'lightpink';
-    console.log(selectedContent);
-    if (selectedContent) span.appendChild(selectedContent);
-    if (selection) selection.deleteContents();
-    selection?.insertNode(span);
-
-    // const range = this.getSelection()?.getRangeAt(0);
-    // if (range) {
-    //   const oldConent = document.createTextNode(range ? range.toString() : '');
-    //   const newElement = document.createElement('span');
-    //   newElement.style.fontSize = '32px';
-    //   newElement.append(oldConent);
-    //   console.log(oldConent);
-    //   range.deleteContents();
-    //   range.insertNode(newElement);
-    // }
-    // this.getSelection().getRangeAt(0).startContainer.previousElementSibling.style['background-color']
-  }
-
-  onItalics(): void {
-    // const selection = this.getSelection()?.getRangeAt(0);
-    // const selectedContent = selection?.extractContents();
-    // const span = document.createElement('span');
-    // span.style.backgroundColor = 'lightpink';
-    // if (selectedContent) span.appendChild(selectedContent);
-    // selection?.insertNode(span);
-
-    const range = this._editorService.getSelection()?.getRangeAt(0);
-    if (range) {
-      const oldConent = document.createTextNode(range ? range.toString() : '');
-      const newElement = document.createElement('span');
-      newElement.style.fontSize = '20px';
-      newElement.append(oldConent);
-      range.deleteContents();
-      range.insertNode(newElement);
-    }
-  }
-
-  remFor(): void {
-    const range = this._editorService.getSelection()?.getRangeAt(0);
-    if (range) {
-      const oldConent = document.createTextNode(range ? range.toString() : '');
-      const newElement = document.createElement('span');
-      newElement.style.fontSize = '32px';
-      newElement.append(oldConent);
-      range.deleteContents();
-      range.insertNode(newElement);
+    switch (action) {
+      case 'bold':
+        this._editorService.executeAction('strong');
+        break;
+      default:
+        break;
     }
   }
 
--- projects/digipad/src/app/editor/editor.service.ts
+++ projects/digipad/src/app/editor/editor.service.ts
@@ -7,13 +7,33 @@ export class EditorService {
   private _window: Window & typeof globalThis = window;
   private _document: Document = document;
 
-  getSelection(): Selection | null {
+  /**
+   * Function to get the current selected text in the editor
+   * That can be either document selection or window selection
+   * @returns {Range | null}
+   */
+  private getSelection(): Selection | null {
     if (this._window && this._window.getSelection()) {
       return this._window.getSelection();
     } else if (this._document && this._document.getSelection()) {
       return this._document.getSelection();
     }
     return null;
+  }
+
+  /**
+   * Function to apply the tag changes
+   * @param action string
+   * @returns void
+   */
+  applyTagChanges(action: string): void {
+    const selection = this.getSelection()?.getRangeAt(0);
+    const tag = document.createElement(action);
+    selection?.surroundContents(tag);
+  }
+
+  executeAction(action: string): void {
+    this.applyTagChanges(action);
   }
 
 }
